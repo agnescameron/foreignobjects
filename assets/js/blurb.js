d18c3e5195b604b2ee11d20575e0986b34ee4f23
agnescameron <agnesfcameron@gmail.com>
2019-09-17 10:11:14 -0400

    tidying

--- assets/js/blurb.js
+++ assets/js/blurb.js
@@ -1,65 +1,68 @@
-var blurb = {
 //json object with "firstPara" and "secondPara" as its keys
-
-	"firstPara" : {
+var blurb = {
 	// an array object with "array0", "array1" as its keys
-		"array0" : ["Foreign", 
-		"Formed by <a target='_blank' href='http://www.agnescameron.info'>Agnes Cameron (UK),</a> <a target='_blank' href='http://www.samtous.wtf'>Sam Ghantous (LB/CA)</a>, <a target='_blank' href='http://www.kalli-retzepi.com'>Kalli Retzepi (GR)</a> & <a target='_blank' href='http://www.zhexi.info'>Gary Zhexi Zhang (UK/CN),</a> Foreign",
-		"Comprising four recent MIT graduates, Foreign",
+	"firstPara" : {
+	// array0 is an array containing a list of sentences e.g. "Foreign"
+		"array0" : [
+			"Foreign", 
+			`Formed by <a target='_blank' href='http://www.agnescameron.info'>Agnes Cameron (UK),</a> 
+				<a target='_blank' href='http://www.samtous.wtf'>Sam Ghantous (LB/CA)</a>, <a target='_blank' 
+				href='http://www.kalli-retzepi.com'>Kalli Retzepi (GR)</a> & <a target='_blank' 
+				href='http://www.zhexi.info'>Gary Zhexi Zhang (UK/CN),</a> Foreign`,
+			"Comprising four recent MIT graduates, Foreign",
 		],
 
-		// array0 is an array containing a list of sentences e.g. "Foreign"
-
-		"array1" : ["Objects", 
-	  	"Objects is a collaborative research studio", 
-	 	"Objects is a cultural consultancy", 
-	 	"Objects is a limited liability company",
-	 	"Objects is an imaginary city",
-	 	"Objects is a collection of non-resident aliens",
-	 	"Objects is a unnecessary institute",
-	  ],
-
-	  "that" : ["that",
-	  ],
-
-		'array2' : ["works", 
-	  "make",
-	  "eat",
-	  "drive",
-	  "shit",
-	  "build",
-	  "clean",
-	  "incubate",
-	  "sleep",
-	  "write",
-	  ],
-
-		'array3' : ["with artists and institutions that",
-	  "websites that",
-	  "field reports that",
-	  "simulation models that",
-	  "critical tools that"
-	  ],
-
-	  'array4' : ["have to live",
-	  "are down",
-	  "sleep in the tall grasses",
-	  "don't beat around the bush",
-	  "engage",
-	  "chew the fat",
-	  "seek to come to an understanding"
-	  ],
-
-	  	'array5' : ["with the internet.", 
-	  	"with emerging technocultures.", 
-	  	"with software and society.",
-	  	"with architecture and media.",
-	  	"with internet health.",
-	  	"with speculative fictions.",
-	  	"with friendly blobs.",
-	  	"with network theory.",
+		"array1" : [
+			"Objects", 
+		  	"Objects is a collaborative research studio", 
+		 	"Objects is a cultural consultancy", 
+		 	"Objects is a limited liability company",
+		 	"Objects is an imaginary city",
+		 	"Objects is a collection of non-resident aliens",
+		 	"Objects is a unnecessary institute",
 	  	],
-	  },
+
+		'array2' : [
+			"works", 
+			"makes",
+			"eats",
+			"drives",
+			"shits",
+			"builds",
+			"cleans",
+			"incubates",
+			"writes",
+	  	],
+
+		'array3' : [
+			"with artists and institutions that",
+			"websites that",
+			"field reports that",
+			"simulation models that",
+			"critical tools that"
+		],
+
+	  	'array4' : [
+			"have to live",
+			"are down",
+			"sleep in the tall grasses",
+			"don't beat around the bush",
+			"engage",
+			"chew the fat",
+			"seek to come to an understanding"
+	  	],
+
+	  	'array5' : [
+		  	"with the internet.", 
+		  	"with emerging technocultures.", 
+		  	"with software and society.",
+		  	"with architecture and media.",
+		  	"with internet health.",
+		  	"with speculative fictions.",
+		  	"with friendly blobs.",
+		  	"with network theory.",
+	  	],
+	},
 
 // "secondPara" : {
 
