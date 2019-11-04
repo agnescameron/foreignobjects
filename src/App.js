25165fd7a7846eca5e1fa02a7769877568227b0f
Kallirroi <kallirroi.retzepi@gmail.com>
2019-11-04 11:25:58 -0500

    seo keywords

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 
 import Home from './routes/Home';
 import About from './routes/About';
@@ -10,6 +11,13 @@ class App extends React.Component {
   render() {
     return (
       <Router>
+
+        <Helmet>
+          <title>FOREIGN OBJECTS</title>
+          <meta name="description" content="A research studio and consultancy based in NYC." />
+          <meta name="keywords" content="design, consultancy, NEW INC, MIT, Media Lab, ACT, decentralization, Kallirroi, Retzepi, Agnes Cameron, Ghantous, Zhexi, architecture, software" />
+        </Helmet>
+
         {/*<Link to="/" className="App-link">Home</Link>*/}
         {/*<Link to="/about" className="App-link-about">About</Link>*/}
         <Route exact path="/" component={Home} />
