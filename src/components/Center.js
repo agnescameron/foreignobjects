62cad983b0d28574b8495273281183c99db83f9d
rokeby <gary.zhexi.zhang@gmail.com>
2020-01-13 01:19:18 -0500

    removed internet archive accidental listing as client

--- src/components/Center.js
+++ src/components/Center.js
@@ -22,40 +22,28 @@ export default class Center extends React.Component{
 	      	<div className="Blurb">
 				<p>
 				<div onClick={() => this.setState({Blurb: false})}>&#8598;</div>
-				<a className="App-link"
-            	href=""
-            	target="_blank"
-            	rel="noopener noreferrer">Foreign Objects</a> is a design studio and consultancy that helps clients execute creative and critical projects in physical, computational and networked space. We are based at NEW INC., at the New Museum in NYC.
+				Foreign Objects is a design studio and consultancy that helps clients execute creative and critical projects in physical, computational and networked space. We are based at NEW INC., at the New Museum in NYC.
 				<br/>
 				<br/>
 				Formed by four foreigners at MIT, we are a collective of interdisciplinary practitioners with expertise in architectural design, web development, electrical engineering and contemporary art. Combining experimentation and research, our work probes at the boundaries of technology, culture and society.      
 				<br/>
 				<br/>
 				Clients we have worked with recently include <a className="App-link"
-            	href=""
+            	href="http://propspaper.com/"
             	target="_blank"
             	rel="noopener noreferrer">PROPS Paper</a>, <a className="App-link"
-            	href=""
+            	href="http://www.tanyabonakdargallery.com/artists/agnieszka-kurant"
             	target="_blank"
             	rel="noopener noreferrer">Agnieszka Kurant</a>, <a className="App-link"
-            	href=""
+            	href="http://www.sakiya.org"
             	target="_blank"
-            	rel="noopener noreferrer">Sakiya</a>, <a className="App-link"
-            	href=""
-            	target="_blank"
-            	rel="noopener noreferrer">N3Plus</a>, <a className="App-link"
-            	href=""
+            	rel="noopener noreferrer">Sakiya</a>, N3Plus, <a className="App-link"
+            	href="https://london.sciencegallery.com/seasons/dark-matter"
             	target="_blank"
             	rel="noopener noreferrer">Science Gallery London</a>, <a className="App-link"
-            	href=""
+            	href="https://www.armitagegonedance.org/"
             	target="_blank"
-            	rel="noopener noreferrer">Karole Armitage</a>, <a className="App-link"
-            	href=""
-            	target="_blank"
-            	rel="noopener noreferrer">Mozilla Foundation</a> and the <a className="App-link"
-            	href=""
-            	target="_blank"
-            	rel="noopener noreferrer">Internet Archive.</a> 
+            	rel="noopener noreferrer">Karole Armitage</a>.</a> 
 				</p>
 
 			</div>
