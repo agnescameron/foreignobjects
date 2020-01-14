95a08ae8697f597988345bf935a2c209891ab144
rokeby <gary.zhexi.zhang@gmail.com>
2020-01-14 01:25:56 -0500

    basic intervalled ticker text implemented

--- src/components/Top.js
+++ src/components/Top.js
@@ -1,38 +1,39 @@
 import React from 'react';
 import './Components.css';
 
+export default class Ticker extends React.Component{
+// counter, state stores count
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentCount: 0
+    };
+  }
 
-export default class Top extends React.Component{
-  render() {
-    const className= this.props.hovering ? 'Top' : 'hidden Top'
-    return (
-      <div className={className}>
-        FOREIGN OBJECTS consists of&nbsp;
-<a
-            className="App-link"
-            href="https://kalli-retzepi.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >kalli retzepi</a>,&nbsp;
-          <a
-            className="App-link"
-            href="http://samtous.wtf"
-            target="_blank"
-            rel="noopener noreferrer"
-          >sam ghantous</a>,&nbsp;
-          <a
-            className="App-link"
-            href="http://zhexi.info"
-            target="_blank"
-            rel="noopener noreferrer"
-          >gary zhexi zhang</a>, and&nbsp;
-          <a
-            className="App-link"
-            href="http://agnescameron.info"
-            target="_blank"
-            rel="noopener noreferrer"
-          >agnes cameron</a>.
-      </div>
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.increment(),
+      5000
     );
   }
+
+  increment() {
+    this.setState({
+      currentCount: (this.state.currentCount + 1) % 5
+      });
+    }
+  
+
+  render() {
+    // const className = this.props.hovering ? 'Top' : 'hidden Top';
+    const textList = ['text one', 'text two', 'text three', 'text four', 'text five'];
+    return (
+      <div className="Ticker">
+          <div>{textList[this.state.currentCount]}</div>
+        </div>
+    );
+  }
+
 }
+
