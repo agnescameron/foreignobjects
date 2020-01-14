20bd4aa1cad9938b6ea767c6b86890d4550d467e
agnescameron <agnesfcameron@gmail.com>
2020-01-14 13:47:24 -0500

    sets projects in state

--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -1,42 +1,22 @@
 import React from 'react';
 import './Projects.css';
 import { AIRTABLE_API_KEY } from '../keys.js'
-
-const Airtable = require('airtable');
-Airtable.configure({
-    endpointUrl: 'https://api.airtable.com',
-    apiKey: AIRTABLE_API_KEY,
-});
-const base = Airtable.base('appJVRlTLOZYKPWBs');
+const MAX_RECORDS = 20;
 
 export default class Projects extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
-      projects: []
+      projects: [],
     }
   }
 
   componentDidMount() {
-    base('Table 1').select({
-        // Selecting the first 3 records in Grid view:
-        maxRecords: 3,
-        view: "Grid view"
-    }).eachPage(function page(records, fetchNextPage) {
-        // This function (`page`) will get called for each page of records.
-
-        records.forEach(function(record) {
-            console.log('Retrieved', record.get('Project Name'));
-        });
-
-        // To fetch the next page of records, call `fetchNextPage`.
-        // If there are more records, `page` will get called again.
-        // If there are no more records, `done` will get called.
-        fetchNextPage();
-
-    }, function done(err) {
-        if (err) { console.error(err); return; }
-    });
+    fetch(`https://api.airtable.com/v0/appJVRlTLOZYKPWBs/Table%201?maxRecords=${MAX_RECORDS}&view=Grid%20view&api_key=${AIRTABLE_API_KEY}` )
+    .then((resp) => resp.json())
+    .then(data => {
+       this.setState({ projects: data.records });
+    }).catch(err => {console.log(err)});
   }
 
   render() {
