16dadea4ca30fc881048b0870c797997b99b9815
rokeby <gary.zhexi.zhang@gmail.com>
2020-01-20 09:14:02 -0500

    projects merge issue fixed

--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -22,25 +22,6 @@ export default class Projects extends React.Component{
         }).catch(err => {console.log(err)});
     }
     
-    render() {
-        return (
-                <div className="App">
-                p r o j e c t s
-                </div>
-                );
-    }
-  }
-
-  componentDidMount() {
-    fetch(`https://api.airtable.com/v0/appJVRlTLOZYKPWBs/Table%201?maxRecords=${MAX_RECORDS}&view=Grid%20view`, {
-     "headers": {"Authorization": `Bearer ${AIRTABLE_API_KEY}`}
-     })
-    .then((resp) => resp.json())
-    .then(data => {
-       this.setState({ projects: data.records });
-    }).catch(err => {console.log(err)});
-  }
-
   render() {
     return (
       <div className="App">
