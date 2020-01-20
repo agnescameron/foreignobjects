dc7175c8d6c91a958ce7ccc6803d6c51201267a2
rokeby <gary.zhexi.zhang@gmail.com>
2020-01-20 13:40:57 -0500

    cleaned project divs

--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -35,30 +35,24 @@ export default class Projects extends React.Component{
     var projects = this.state.projects;
     return (
       <div className="App">
-        <div className="projectsContainer">
           <Top hovering="true" />
-          <div className="Projects">
-              <div className="mobileHeader">FOREIGN OBJECTS</div>
-              <div className="mobileNavContainer">
-                <div className="mobileTitleLeft">
-                <a className="Orange Arrow"
-                href="https://www.foreignobjects.net"
-                rel="noopener noreferrer">&#8598;</a>HOME</div>
-                <div className="mobileTitleCenter">CONVERSATION<span className="Orange Arrow">&#8600;</span></div>
-                <div className="mobileTitleRight">ABOUT<span className="Orange Arrow">&#8594;</span></div>
-              </div>  
-            <div className="backgroundLogo" draggable="false">
-              <div>FOREIGN OBJECTS</div>
-            </div>
-              <div className="Verso">
-                <div className="projectBox">
-                {projects.map((project, i) => {
-                    // Return the element. Also pass key
-                   return (<ProjectModule key={i} project={project} />)
-                })}
-              </div>
-            </div>
+          <div className="mobileHeader">FOREIGN OBJECTS</div>
+          <div className="mobileNavContainer">
+            <div className="mobileTitleLeft">
+            <a className="Orange Arrow"
+            href="https://www.foreignobjects.net"
+            rel="noopener noreferrer">&#8598;</a>HOME</div>
+            <div className="mobileTitleCenter">CONVERSATION<span className="Orange Arrow">&#8600;</span></div>
+            <div className="mobileTitleRight">ABOUT<span className="Orange Arrow">&#8594;</span></div>
+          </div>  
+          <div className="backgroundLogo" draggable="false">
+            FOREIGN OBJECTS
           </div>
+          <div className="projectsContainer">
+              {projects.map((project, i) => {
+                  // Return the element. Also pass key
+                 return (<ProjectModule key={i} project={project} />)
+              })}
         </div>
       </div>
     );
