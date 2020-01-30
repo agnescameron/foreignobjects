1c596a4a6d27e4d7b5d110a53862113839fc7695
rokeby <gary.zhexi.zhang@gmail.com>
2020-01-29 22:56:19 -0500

    IP test

--- src/components/IP.js
+++ src/components/IP.js
@@ -21,35 +21,34 @@ componentDidMount(){
 
     publicIP()
     .then(ip => {
-      console.log(ip);
-      // '47.122.71.234'
+      console.log("ip");
       this.setState({ ip : ip })
-    })
-    // uses iplocation to fetch ipapi IP location API data
-        // .then(
-        //     fetch(cors + ipinfo + this.state.ip + '/json')
-        //     // return ipresponse.json from API and set it to ipData prop
-        //     .then(
-        //       (ipresponse) => {
-        //     return ipresponse.json();
-        //     }).then(
-        //       (ipresponse) => {
-        //       // console.log(ipresponse);
-        //       this.setState({ ipData : ipresponse });
-        //       })
-        //     )
-    .catch(error => {
+    }).catch(error => {
       console.log(error);
       // 'Unable to get IP address.'
-    })    
-}
+    })
+    // uses iplocation to fetch ipapi IP location API data
+        .then(
+            fetch(cors + ipinfo + this.state.ip + '/json')
+            // return ipresponse.json from API and set it to ipData prop
+            .then(
+              (ipresponse) => {
+            return ipresponse.json();
+            }).then(
+              (ipresponse) => {
+              // console.log(ipresponse);
+              this.setState({ ipData : ipresponse });
+              })
+            )
+    }
 
   render() {
      const ipData = this.state.ipData;
      console.log("ipData is", ipData)
     return (
-      <div></div>
-
+ <div className="ipAddress">
+            {this.state.ipData.city}... {this.state.ipData.region}... {this.state.ipData.country}... {this.state.ipData.longitude}&deg; N, {this.state.ipData.latitude}&deg; E, {this.state.ipData.org} 
+    </div>
     );
   }
 }
